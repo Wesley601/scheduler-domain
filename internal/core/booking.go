1d5f0303acc152298fcf3610881f193fe6d18a5b
wesley601 <wesleypassos601@gmail.com>
2023-10-06 08:56:45 -0300

    refactor: add core pkg

--- internal/core/booking.go
+++ internal/core/booking.go
@@ -1,4 +1,4 @@
-package schedule
+package core
 
 type Booking struct {
 	Window Window
--- main.go
+++ main.go
@@ -4,25 +4,25 @@ import (
 	"fmt"
 	"time"
 
-	"alinea.com/internal/schedule"
+	"alinea.com/internal/core"
 	"alinea.com/pkg/utils"
 )
 
 func main() {
-	service := schedule.Service{
+	service := core.Service{
 		Name:     "My Service",
 		Duration: utils.Must(time.ParseDuration("5m")),
 	}
 
-	s := schedule.Schedule{
+	s := core.Schedule{
 		Name: "My Schedule",
-		Slots: []schedule.Slot{
-			utils.Must(schedule.NewSlot(time.Monday, "10:00:00", "10:45:00")),
-			utils.Must(schedule.NewSlot(time.Tuesday, "11:00:00", "11:45:00")),
+		Slots: []core.Slot{
+			utils.Must(core.NewSlot(time.Monday, "10:00:00", "10:45:00")),
+			utils.Must(core.NewSlot(time.Tuesday, "11:00:00", "11:45:00")),
 		},
 	}
 
-	w, err := s.ListAvailableSlots(schedule.Window{
+	w, err := s.ListAvailableSlots(core.Window{
 		From: utils.Must(time.Parse(time.RFC3339, "2023-10-01T00:00:00Z")),
 		To:   utils.Must(time.Parse(time.RFC3339, "2023-10-05T00:00:00Z")),
 	}, service)
@@ -35,8 +35,8 @@ func main() {
 		fmt.Printf("%s - %s %s\n", w2.From.Format(time.RFC3339), w2.To.Format(time.RFC3339), w2.From.Weekday())
 	}
 
-	result, err := s.Fits(schedule.Booking{
-		Window: schedule.Window{
+	result, err := s.Fits(core.Booking{
+		Window: core.Window{
 			From: utils.Must(time.Parse(time.RFC3339, "2023-10-03T15:30:00Z")),
 			To:   utils.Must(time.Parse(time.RFC3339, "2023-10-03T15:35:00Z")),
 		},
